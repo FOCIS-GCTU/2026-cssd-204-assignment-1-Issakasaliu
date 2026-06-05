2b9cf58de048df7364019c91649303a3b077335b
Issakasaliu <2425402813@live.gctu.edu.gh>
2026-06-05 06:33:06 -0500

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+         #File:initials.py
+        #Description:Print out my initials
+        #assignment Number:assignment:1
+        #
+        #Name:ISSAKA SALIU
+        #STUDENT ID:2425402813
+        #Email:saliuissaka5@gmail.com
+        #Grader:AUGUSTUS BUCKMAN
+        #
+        #On my honor,ISSAKA SALIU,this programming assignment is my own work
+        # and i have not provided this code to any other student.
+def main ():
+        #Python program yto print my initials large
+        print()
+        print("...IDS")
+        print()
 
-def main():
-  pass #code goes here
+        print("...IIIIIIIIIIII........DDDDDDDD.............SSSSSSSSSSS.....")
+        print("...IIIIIIIIIIII........DDDDDDDDDD..........SSSSSSSSSSSS.....")
+        print(".......IIII............DD......DDD........SSSS..............")
+        print(".......IIII............DD.......DD........SSSS..............")
+        print(".......IIII............DD.......DD..........SSSSSSS.........")
+        print(".......IIII............DD.......DD...........SSSSSSS........")
+        print(".......IIII............DD.......DD................SSSS......")
+        print(".......IIII............DD......DDD................SSSSS.....")
+        print("...IIIIIIIIIIII...**...DDDDDDDDDD....**...SSSSSSSSSSS.....**")
+        print("...IIIIIIIIIIII...**...DDDDDDDDD.....**...SSSSSSSSSSS.....**")
 
-
-
+        print()
 main()
